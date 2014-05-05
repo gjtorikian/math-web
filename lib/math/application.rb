acdf77988bf193c8f9f35e7c169a55ed833b1323
Brandon Keepers <brandon@opensoul.org>
2014-05-05 11:21:19 -0500

    Add mode param

--- lib/math/application.rb
+++ lib/math/application.rb
@@ -6,13 +6,19 @@ module Math
     set :views, root.join('views')
     set :public, root.join('public')
 
+    Modes = {
+      'inline'    => lambda { |equation|  "$#{equation}$" },
+      'displayed' => lambda { |equation|  "$$#{equation}$$" }
+    }
+
     get "/" do
       erb :index
     end
 
     get "/formula" do
+      mode = Modes[params['mode']] || Modes['inline']
       content_type 'image/svg+xml'
-      to_svg(params[:formula])
+      to_svg(mode.call(params['formula']))
     end
 
   private
@@ -23,7 +29,7 @@ module Math
 
     def to_svg(formula)
       tempfile = Tempfile.new('mathematical')
-      status = mathmatical.process("$#{formula}$", tempfile.path)
+      status = mathmatical.process(formula, tempfile.path)
       raise RuntimeError unless status
       tempfile.rewind
       tempfile.read
